99198f66d3f1afe52be0e2ed9ae428ab2f8187fb
Ankie121 <ankierao220@gmail.com>
2024-12-31 18:34:36 +0530

    cookie can receive on another page

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const cookieParser = require("cookie-parser");
 var bcrypt = require('bcrypt');
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 3000;
 
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash("babybabybaby", salt, function (err, hash) {
-        console.log(hash);
-    });
-  });
-  res.send('hello')
+  const token = jwt.sign({email:"ankit@example.com"},"secret")
+  res.cookie('token', token);
+  // console.log(token);
+  res.send('received')
 });
+
+app.get("/read",(req,res) => {
+  let data = jwt.verify(req.cookies.token,"secret");
+  console.log(data);
+  res.send('done');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
